e2053a6455ec0d5e075507825cb6ebd17d6d5361
Josh Bruce <brucey125@hotmail.co.uk>
2011-10-01 22:19:39 +0100

    Cart class is fully functional

--- config/cart.class.php
+++ config/cart.class.php
@@ -48,30 +48,42 @@ class Cart {
 	
 	public function setItemQuantity($product_id, $quantity) {
 		if (isset($product_id) && isset($quantity)) {
-		
+			if (array_key_exists($product_id, $_SESSION['cart'])) {
+				$_SESSION['cart'][$product_id]['product_quantity'] = $quantity;
+			}
 		}
 	}
 	
 	public function getItemName($product_id) {
 		if (isset($product_id)) {
-		
+			if (array_key_exists($product_id, $_SESSION['cart'])) {
+				return $_SESSION['cart'][$product_id]['product_name'];
+			}
 		}
 	}
 	
 	public function getItemPrice($product_id) {
 		if (isset($product_id)) {
-		
+			if (array_key_exists($product_id, $_SESSION['cart'])) {
+				return $_SESSION['cart'][$product_id]['product_price'];
+			}
 		}
 	}
 	
 	public function getItemQuantity($product_id) {
 		if (isset($product_id)) {
-		
+			if (array_key_exists($product_id, $_SESSION['cart'])) {
+				return $_SESSION['cart'][$product_id]['product_quantity'];
+			}
 		}
 	}
 	
 	public function getTotalPrice() {
 		$this->totalPrice = 0;
+		foreach ($_SESSION['cart'] as $cart) {
+			$this->totalPrice += $cart['product_quantity'] * $cart['product_price'];
+		}
+		return $this->totalPrice;
 	}
 	
 	public function showCartArray() {

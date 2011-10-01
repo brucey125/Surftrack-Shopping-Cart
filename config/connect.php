47cfec0334ad1400ba53287cf3e2881e3124469c
Josh Bruce <brucey125@hotmail.co.uk>
2011-10-01 20:03:22 +0100

    Config files

---
+++ config/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "config.php";
+
+$db_new_connect = mysql_connect($config['db_localhost'],$config['db_username'],$config['db_password']);
+$db_select = mysql_select_db($config['db_name'],$db_new_connect);
+
+if (!$db_new_connect || !$db_select) {
+	die(mysql_error());
+}
+
+?>

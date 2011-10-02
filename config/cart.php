9356c9ce56ec7102652f89bf1de0c8e3047656d5
Josh Bruce <brucey125@hotmail.co.uk>
2011-10-02 12:53:48 +0100

    Changed POST to GET

--- config/cart.php
+++ config/cart.php
@@ -8,23 +8,23 @@ if (isset($_GET['action'])) {
 	$action = $_GET['action'];
 	switch ($action) {
 		case "addItem":
-			if (isset($_POST['item_id'])) {
-				$id = (int) $_POST['item_id'];
+			if (isset($_GET['item_id'])) {
+				$id = (int) $_GET['item_id'];
 				$Cart->addItem($id);
 			}
 		break;
 		
 		case "removeItem":
-			if (isset($_POST['item_id'])) {
-				$id = (int) $_POST['item_id'];
+			if (isset($_GET['item_id'])) {
+				$id = (int) $_GET['item_id'];
 				$Cart->removeItem($id);
 			}
 		break;
 		
 		case "setItemQuantity":
-			if (isset($_POST['item_id']) && isset($_POST['quantity'])) {
-				$id = (int) $_POST['item_id'];
-				$quantity = (int) $_POST['quantity'];
+			if (isset($_GET['item_id']) && isset($_GET['quantity'])) {
+				$id = (int) $_GET['item_id'];
+				$quantity = (int) $_GET['quantity'];
 				$Cart->setItemQuantity($id, $quantity);
 			}
 		break;
@@ -32,5 +32,6 @@ if (isset($_GET['action'])) {
 }
 
 $Cart->showCartArray();
+echo $Cart->getTotalPrice();
 
 ?>

da2bc9247ff83663d41a5bd25efb07f81ab4dcb3
Josh Bruce <brucey125@hotmail.co.uk>
2011-10-02 12:56:38 +0100

    Deleted old file and testing index.php cart options

--- index.php
+++ index.php
@@ -17,7 +17,11 @@ require_once("config/connect.php");
 <body>
 
 	<?php
-		
+		$products = mysql_query("SELECT * FROM products");
+		while ($row = mysql_fetch_assoc($products)) {
+			echo '<a href="config/cart.php?action=addItem&item_id='.$row['product_id'].'"><input type="button" id="'.$row['product_id'].'" value="Add '.$row['product_name'].'" /></a>';
+			echo '<a href="config/cart.php?action=removeItem&item_id='.$row['product_id'].'"><input type="button" id="'.$row['product_id'].'" value="Remove '.$row['product_name'].'" /><br /></a>';
+		}
 	?>
 
 </body>
